92e28f57bee3c248c195e6919afa44483c3b11fb
Andrey <crocodile8000@gmail.com>
2017-05-10 00:11:56 +0300

    postOnUi()

--- lib/src/main/java/crocodile8008/common/ThreadUtil.java
+++ lib/src/main/java/crocodile8008/common/ThreadUtil.java
@@ -1,6 +1,9 @@
 package crocodile8008.common;
 
+import android.content.Context;
+import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.NonNull;
 
 /**
  * Created by Andrei Riik in 09.05.2017.
@@ -10,5 +13,10 @@ public class ThreadUtil {
 
     public static boolean isMainThread() {
         return Looper.myLooper() == Looper.getMainLooper();
+    }
+
+    public static void postOnUi(@NonNull Runnable task, @NonNull Context context) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(task);
     }
 }
