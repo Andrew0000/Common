c214e732843717c560c6c40a1ea697859e3d0ee4
Andrey <crocodile8000@gmail.com>
2017-05-11 22:52:46 +0300

    ThrowIfAllowed

---
+++ lib/src/main/java/crocodile8008/common/ThrowIfAllowed.java
@@ -0,0 +1,29 @@
+package crocodile8008.common;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Andrei Riik in 11.05.2017.
+ */
+
+@SuppressWarnings("unused")
+public class ThrowIfAllowed {
+
+    private static boolean failEnabled;
+
+    /**
+     * Just invoke this with actual BuildConfig.DEBUG value.
+     */
+    public static void setFailEnabled(boolean failEnabled) {
+        ThrowIfAllowed.failEnabled = failEnabled;
+    }
+
+    /**
+     * Throws given exception if allowed, do nothing otherwise.
+     */
+    public static void runtime(@NonNull RuntimeException exception) {
+        if (failEnabled) {
+            throw exception;
+        }
+    }
+}
